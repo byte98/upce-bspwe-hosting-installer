2500b57581383a9566679f525587d6a18daecbc4
Jiří Škoda <developer@skodaj.cz>
2024-02-21 13:49:00 +0100

    Configuration of web server started

--- install.ps1
+++ install.ps1
@@ -97,13 +97,23 @@ if (Get-UserConfirmation){ # User declared SSH installed and running
 
             # Install required packages
             Request-Command -description "Installing Apache2 web server" -command "dnf install httpd -y" -exitMessage "❗️ ERROR: Installation of Apache2 failed!" -exitCode 20 -session $session -start $startTime -successStr $success -failStr $fail 
-            Request-Command -description "Installing PHP processor" -command "dnf install httpd -y" -exitMessage "❗️ ERROR: Installation of PHP failed!" -exitCode 21 -session $session -start $startTime -successStr $success -failStr $fail
-            Request-Command -description "Installing PostgreSQL database" -command
+            #Request-Command -description "Installing PHP processor" -command "dnf install httpd -y" -exitMessage "❗️ ERROR: Installation of PHP failed!" -exitCode 21 -session $session -start $startTime -successStr $success -failStr $fail
+            #Request-Command -description "Installing PostgreSQL database" -command
+
+            # Set up default web page application
+            Print-Text -type "ℹ️ " -content "Installer now configures web application providing user interface of Simple Hosting."
+            $address = Read-Host -Prompt "Enter address of server (example: www.example.com)"
+
+            #
 
             # Set up firewall
             Request-Command -description "Allowing HTTP through firewall" -command "firewall-cmd --add-service=http --permanent" -exitMessage "❗️ ERROR: Cannot add serivce HTTP to the firewall!" -exitCode 30 -session $session -start $startTime -successStr $success -failStr $fail 
             Request-Command -description "Allowing HTTPS through firewall" -command "firewall-cmd --add-service=https --permanent" -exitMessage "❗️ ERROR: Cannot add serivce HTTPS to the firewall!" -exitCode 31 -session $session -start $startTime -successStr $success -failStr $fail 
             Request-Command -description "Restarting firewall" -command "firewall-cmd --reload" -exitMessage "❗️ ERROR: Restarting of firewall failed!" -exitCode 32 -session $session -start $startTime -successStr $success -failStr $fail 
+
+            # Set up services
+            Request-Command -description "Starting HTTPd service" -command "systemctl start httpd.service" -exitMessage "❗️ ERROR: Starting of httpd service failed!" -exitCode 40 -session $session -start $startTime -successStr $success -failStr $fail
+            Request-Command -description "Configuring auto-start of HTTPd service" -command "systemctl start httpd.service" -exitMessage "❗️ ERROR: Configuring of auto-start of httpd service failed!" -exitCode 40 -session $session -start $startTime -successStr $success -failStr $fail
 
             Exit-Script -start $startTime -code 0 -message "✅ Script successfully installed Simple Hosting on the server."
         }
